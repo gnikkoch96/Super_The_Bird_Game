4ea12880965fd4a9c9e580df5a053130c28ba00d
Khai Trinh <khaitrinh99@gmail.com>
2021-01-23 16:11:08 -0800

    Initial commit.

---
+++ Super The Bird Game/desktop/src/com/procodenators/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.procodenators.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.procodenators.game.TheBirdGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheBirdGame(), config);
+	}
+}
