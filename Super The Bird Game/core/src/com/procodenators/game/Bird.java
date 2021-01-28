ffe6a726266fa0854a73fcf387fc95484fcc8959
Khai Trinh <khaitrinh99@gmail.com>
2021-01-27 16:18:37 -0800

    Uploaded assets and attempted resize function.

---
+++ Super The Bird Game/core/src/com/procodenators/game/Bird.java
@@ -0,0 +1,37 @@
+package com.procodenators.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Bird {
+
+    private Texture[] idlebird;
+
+    public Bird() {
+        idlebird = new Texture[4];
+        for(int i = 1; i < 5; i++) {
+            String path = "Bird_Animations//idle bird ";
+            path = path.concat(Integer.toString(i));
+            path = path.concat(".png");
+            Texture temp = resize(path, 250, 250);
+            idlebird[i-1] = temp;
+        }
+    }
+
+    public Texture resize(String path, int newWidth, int newHeight) {
+        Pixmap original = new Pixmap(Gdx.files.internal(path));
+        Pixmap resized = new Pixmap(newWidth, newHeight, original.getFormat());
+        resized.drawPixmap(original,
+                0, 0, original.getWidth(), original.getHeight(),
+                0, 0, resized.getWidth(), resized.getHeight());
+        Texture output = new Texture(resized);
+        original.dispose();
+        resized.dispose();
+        return output;
+    }
+
+    public Texture[] getIdlebird() {
+        return idlebird;
+    }
+}
--- Super The Bird Game/core/src/com/procodenators/game/TheBirdGame.java
+++ Super The Bird Game/core/src/com/procodenators/game/TheBirdGame.java
@@ -3,25 +3,31 @@ package com.procodenators.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TheBirdGame extends ApplicationAdapter {
-	public static final int WIDTH = 480;
-	public static final int HEIGHT = 800;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 480;
 
 	public static final String TITLE = "The Bird Game";
 
 	private GameController gc;
 
 	SpriteBatch batch;
-	Texture img;
+	Texture bg;
+
+	Bird bird;
+	Texture[] idlebird;
 	
 	@Override
 	public void create () {
 		gc = new GameController();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		bg = new Texture("Background//bg.png");
+		bird = new Bird();
+		idlebird = bird.getIdlebird();
 	}
 
 	@Override
@@ -30,13 +36,13 @@ public class TheBirdGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bg, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		bg.dispose();
 	}
 }
