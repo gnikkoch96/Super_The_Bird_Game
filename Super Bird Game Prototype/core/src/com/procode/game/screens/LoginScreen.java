d99640b9f322f11ad017f0545613363f024b61d9
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2021-02-24 02:20:56 -0800

    I fixed the login, added some stuff in the gradle

--- Super Bird Game Prototype/core/src/com/procode/game/screens/LoginScreen.java
+++ Super Bird Game Prototype/core/src/com/procode/game/screens/LoginScreen.java
@@ -3,6 +3,7 @@ package com.procode.game.screens;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,6 +12,7 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -28,19 +30,64 @@ public class LoginScreen extends ApplicationAdapter implements Screen {
     private Stage stage;
     private TextButton btnLogin;
     private TextField txtUserName;
+    private TextureAtlas atlas;
     private TextField txtPassword;
+    private Table table;
     private BitmapFont font;
-
+    private FreeTypeFontGenerator fontGenerator;
+    private FreeTypeFontGenerator.FreeTypeFontParameter fontParameter;
     public LoginScreen(SuperBirdGame g){
         game = g;
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
-        Skin skin = new Skin(Gdx.files.internal("glassy-ui.json"));
+        //atlas = new TextureAtlas("font-export.fnt");
+        Skin skin = new Skin(Gdx.files.internal("comic-ui.json"));
+        //Skin skin = new Skin(atlas);
         //Skin skin = new Skin(Gdx.files.internal("uiskin.json"), new TextureAtlas(Gdx.files.internal("uiskin.atlas")));
         //create a text button and send it to skin
+
+
+        fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("Cartoon 2 US.ttf"));
+        fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParameter.size = 80;
+        font = fontGenerator.generateFont(fontParameter);
+        //this is for the user enter username
+        TextField.TextFieldStyle ts = new TextField.TextFieldStyle();
+        ts = skin.get(TextField.TextFieldStyle.class);
+        ts.font = font;
+        txtUserName = new TextField("", ts);
+        //font.getData().setScale(1.0f);
+
+        txtUserName.setSize(800,100);
+
+
+
+
+        //txtUserName.getStyle().font.getData().scale(3);
+        //txtUserName.getStyle().font.getData().setScale(8.0f);
+        //.getFont("default-font").setScale(0.33f, 0.33f);
+        txtUserName.setPosition(game.ANDROID_WIDTH/2 - 500,(game.ANDROID_HEIGHT/2) - 50);
+
+        //txtUserName.set
+
+        //add the user name in the stage
+        stage.addActor(txtUserName);
+
+        txtPassword = new TextField("", ts);
+        txtPassword.setPasswordCharacter('*');
+        txtPassword.setPosition(game.ANDROID_WIDTH/2 - 505,(game.ANDROID_HEIGHT/2) - 150);
+        txtPassword.setPasswordMode(true);
+        txtPassword.setSize(800,100);
+
+        //then send the password into the stage
+        stage.addActor(txtPassword);
+
         TextButton btnLogin = new TextButton("Log In", skin);
+        font = new BitmapFont(Gdx.files.internal("font-export.fnt"), false);
+        font.getData().setScale(2,2);
+        skin.add("fonts", font);
         //set the position and size of the button
-        btnLogin.setPosition(300,130);
+        btnLogin.setPosition(game.ANDROID_WIDTH/2 - 505,(game.ANDROID_HEIGHT/2) - 250);
         btnLogin.setSize(200,100);
 
         //set the listener for log in button
@@ -50,28 +97,6 @@ public class LoginScreen extends ApplicationAdapter implements Screen {
                 btnLoginClicked();
             }
         });
-
-
-        //this is for the user enter username
-        txtUserName = new TextField("", skin.get(TextField.TextFieldStyle.class));
-        //font.getData().setScale(1.0f);
-        txtUserName.getStyle().font.getData().setScale(4.0f,4.0f);
-                //.getFont("default-font").setScale(0.33f, 0.33f);
-        txtUserName.setPosition(game.ANDROID_WIDTH/2 - 500,(game.ANDROID_HEIGHT/2) - 100);
-        txtUserName.setSize(800,160);
-        //txtUserName.set
-
-        //add the user name in the stage
-        stage.addActor(txtUserName);
-
-        txtPassword = new TextField("", skin);
-        txtPassword.setPasswordCharacter('*');
-        txtPassword.setPosition(300,250);
-        txtPassword.setPasswordMode(true);
-        txtPassword.setSize(300,40);
-
-        //then send the password into the stage
-        stage.addActor(txtPassword);
 
         //then draw it on the screen by adding it to stage
         stage.addActor(btnLogin);
