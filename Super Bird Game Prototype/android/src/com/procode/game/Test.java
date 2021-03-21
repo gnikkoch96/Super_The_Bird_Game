3dd8cb4cb0344e5dbcd5345396ae4542b5be6ca7
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2021-03-21 15:43:18 -0700

    d

--- Super Bird Game Prototype/android/src/com/procode/game/Test.java
+++ Super Bird Game Prototype/android/src/com/procode/game/Test.java
@@ -5,6 +5,13 @@ import android.content.Intent;
 import android.widget.Toast;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -13,14 +20,32 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 import java.util.HashMap;
 
 import androidx.annotation.NonNull;
 
-public class Test extends ApplicationAdapter {
+public class Test extends ApplicationAdapter implements Screen {
 
+    private Texture background;
+    private SpriteBatch batch;
     public Test(){
-        onCreate();
+        //onCreate();
+     //   background = resize("background stuff/bg.png", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+      // batch = new SpriteBatch();
+
+    }
+
+    public static Texture resize(String path, int newWidth, int newHeight) {
+        Pixmap original = new Pixmap(Gdx.files.internal(path));
+        Pixmap resized = new Pixmap(newWidth, newHeight, original.getFormat());
+        resized.drawPixmap(original,
+                0, 0, original.getWidth(), original.getHeight(),
+                0, 0, resized.getWidth(), resized.getHeight());
+        Texture output = new Texture(resized);
+        original.dispose();
+        resized.dispose();
+        return output;
     }
 
     public void onCreate(){
@@ -30,7 +55,7 @@ public class Test extends ApplicationAdapter {
         rootRef = FirebaseDatabase.getInstance().getReference();
 
         final HashMap<String,Object> userdataMap = new HashMap<>();
-        userdataMap.put("username", "kev");
+        userdataMap.put("username", "Mason");
         userdataMap.put("password", "123");
         userdataMap.put("email", "email");
         userdataMap.put("name", "fullname");
@@ -39,7 +64,7 @@ public class Test extends ApplicationAdapter {
         rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                rootRef.child("Users").child("Nikko").updateChildren(userdataMap).
+                rootRef.child("Users").child("Alexa").updateChildren(userdataMap).
                         addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
@@ -65,5 +90,26 @@ public class Test extends ApplicationAdapter {
 
             }
         });
+    }
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+       // batch.begin();
+       //this.batch.draw(background, 0, 0);
+        //batch.end();
+    }
+
+    @Override
+    public void hide() {
+
     }
 }
---
+++ Super Bird Game Prototype/android/src/com/procode/game/User.java
@@ -0,0 +1,31 @@
+package com.procode.game;
+
+public class User {
+
+    private user
+
+    public void setUserName(String e){
+        username = e;
+    }
+
+    public void setPassword(){
+
+    }
+
+    public void setEmail(){
+
+    }
+
+    public void setBirthday(){}
+
+    public void setFullName(){}
+
+    public String getBirthday(){return "";}
+    public String getPassword(){return "";}
+    public String getEmail(){return "";}
+    public String getFullName(){return "";}
+    public String getUserName(){return "";}
+
+
+}
+
