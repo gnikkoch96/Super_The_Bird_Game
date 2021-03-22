d4798f1176e5364ad66d37cb8c85ca6dbf6a4c02
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2021-03-22 16:09:21 -0700

    Database updation

--- Super Bird Game Prototype/android/src/com/procode/game/Database.java
+++ Super Bird Game Prototype/android/src/com/procode/game/Database.java
@@ -60,7 +60,88 @@ public class Database {
     }
 
 
+    //=========================This Section if for Updating The Following===========================
+    //=====================Username, Password, Birthday, Email, FullName============================
+    //==============================================================================================
+
+    //update the username from the database
     public void upDateUsername(String username){
 
+        rootRef.child("Users").child(this.username).child(this.username).setValue(username)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+
+        rootRef.child("Users").child(this.username).setValue(username)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
     }
+
+    //update the password from the database
+    public void upDatePassword(String password){
+        rootRef.child("Users").child(this.username).child(this.password).setValue(password)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+    }
+
+    //update the email from the database
+    public void upDateEmail(String email){
+        rootRef.child("Users").child(this.username).child(this.email).setValue(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+    }
+
+    //update the birthday from the database
+    public void upDateBirthday(String birthday){
+        rootRef.child("Users").child(this.username).child(this.birthday).setValue(birthday)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+    }
+
+    //update the fullname from the database
+    public void upDateFullName(String fullname){
+        rootRef.child("Users").child(this.username).child(this.fullName).setValue(fullname)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                    }
+                });
+    }
+
+    public void upDateScore(int score){
+        
+    }
+    //===================================End of Updates ============================================
 }
